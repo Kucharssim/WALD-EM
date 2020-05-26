64533de0c3fd94a383d6962d866257acb6e34b3a
Kucharssim <kucharssim@gmail.com>
2020-05-26 18:51:06 +0200

    plot example of horizontal bias

--- figures/plot_horizontal.R
+++ figures/plot_horizontal.R
@@ -1,11 +1,12 @@
 library(rstan)
 library(tidyverse)
 source(here::here("R", "expose_helpers_stan.R"))
+source(here::here("R", "colours.R"))
 
-
-x_prev <- 100; y_prev <- 100
+x_prev <- 400; y_prev <- 300
 x_min <- 1; x_max <- 800; y_min <- 1; y_max <- 600
-display <- tidyr::expand_grid(x = seq(x_min, x_max, by = 10), y = seq(y_min, y_max, by = 10))
+resolution <- 10
+display <- tidyr::expand_grid(x = seq(x_min, x_max, by = resolution), y = seq(y_min, y_max, by = resolution))
 display$angle           <- NA
 display$radius          <- NA
 display$dist_to_border  <- NA
@@ -18,11 +19,13 @@ for(i in 1:nrow(display)){
   display$angle[i]          <- res[1]
   display$radius[i]         <- res[2]
   display$dist_to_border[i] <- res[3]
-  display$log_lik[i]        <- direction_bias_lpdf(res[1], res[2], res[3], rep(1/2, 2), c(0, pi), rep(8, 2))
+  display$log_lik[i]        <- direction_bias_lpdf(res[1], res[2], res[3], rep(1/2, 2), c(0, pi), rep(15, 2))
   pb$tick()$print()
 }
 
-display$lik <- exp(display$log_lik)
+display$lik       <- exp(display$log_lik)
+display$log_joint <- display$log_lik + log(display$radius)
+display$joint     <- exp(display$log_joint)
 
 ggplot2::ggplot(display, ggplot2::aes(x = x, y = y)) + 
   ggplot2::geom_tile(ggplot2::aes(fill = angle))
@@ -39,29 +42,27 @@ ggplot2::ggplot(display, ggplot2::aes(x = x, y = y)) +
 ggplot2::ggplot(display, ggplot2::aes(x = x, y = y)) + 
   ggplot2::geom_tile(ggplot2::aes(fill = lik))
 
+ggplot2::ggplot(display, ggplot2::aes(x = x, y = y)) + 
+  ggplot2::geom_tile(ggplot2::aes(fill = log_joint))
 
-## check that the pdf integrates to 1
+ggplot2::ggplot(display, ggplot2::aes(x = x, y = y)) + 
+  ggplot2::geom_tile(ggplot2::aes(fill = joint)) +
+  ggplot2::theme_void() +
+  ggplot2::scale_fill_continuous(name = expression(f(theta, r)))
+
+ggplot2::ggsave("horizontal_example.jpg", path = here::here("figures"), width = 0.6*8.5, height = 0.6*6)
+
+## check that the pdf integrates to about 1
 library(pracma)
-library(circular)
 
 integrand <- function(x, y){
   res <- calc_angle_border(x, y, x_prev, y_prev, x_min, x_max, y_min, y_max)
   #r <- sqrt((x-x_prev)^2 + (y-y_prev)^2)
   
-  lpdf <- direction_bias_lpdf(res[1], res[2], res[3], rep(1/2, 2), c(0, pi), rep(8, 2))
+  lpdf <- direction_bias_lpdf(res[1], res[2], res[3], rep(1/2, 2), c(0, pi), rep(15, 2))
   
   exp(lpdf)
 }
 
 int <- pracma::integral2(fun = integrand, xmin = x_min, xmax = x_max, ymin = y_min, ymax = y_max, vectorized = FALSE)
 int
-
-integrand <- function(x, y, max_r){
-  r <- x; theta <- y
-  d <- circular::dvonmises(x = theta, mu = pi/2, kappa = 8, log = FALSE)
-  d <- d * dunif(x = r, min = 0, max = max_r, log = FALSE)
-  
-  return(d)
-}
-
-int <- pracma::integral2(fun = integrand, xmin = 0, xmax = 10, ymin = 0, ymax = 2*pi, max_r = 10, vectorized = FALSE)
